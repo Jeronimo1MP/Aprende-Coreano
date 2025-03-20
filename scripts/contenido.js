a11b198fb7ce35cfd4a184bf32a261bba41621ce
Jeronimo1MP <jeronimom1235@gmail.com>
2025-03-20 13:44:24 -0600

    Eliminar boton experimental

--- scripts/contenido.js
+++ scripts/contenido.js
@@ -3,23 +3,3 @@ const audios = [
     "audios/buenos dias.mp3", "audios/ㄴ.mp3", "audios/ㅁ.mp3", "audios/ㅇ.mp3", "audios/ㅋ.mp3", "audios/ㅎ.mp3",
     "audios/hola.mp3", "audios/ㄷ.mp3", "audios/ㅂ.mp3", "audios/ㅈ.mp3", "audios/ㅌ.mp3"
 ];
-
-function xd() {
-    audios.forEach(n => {
-        n.play().catch(error => console.error("Error al reproducir:", error));
-    });
-}
-
-function reproducir(numero) {
-    let audio = new Audio(audios[numero]);
-    audio.play();
-}
-
-let nuevoElemento = document.createElement("button");
-nuevoElemento.textContent = "Hola, soy un nuevo elemento";
-document.body.appendChild(nuevoElemento);
-
-nuevoElemento.addEventListener("click", function() {
-    reproducir(2);
-});
-
